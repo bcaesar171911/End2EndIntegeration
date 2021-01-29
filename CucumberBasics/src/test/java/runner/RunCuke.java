b909f6ed059f76bdcfa28b20e71938111fccd62a
Bennie <test@t.com>
2021-01-28 22:03:19 -0500

    Updated Runner file

--- CucumberBasics/src/test/java/runner/RunCuke.java
+++ CucumberBasics/src/test/java/runner/RunCuke.java
@@ -1,55 +1,38 @@
 package runner;
 
 import java.io.File;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
+import java.io.IOException;
+import org.apache.commons.io.FileUtils;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
-import com.cucumber.listener.ExtentCucumberFormatter;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
-import utils.SeleniumDriver;
 
 
 @CucumberOptions(
-        plugin = {"json:target/positive/cucumber.json", "pretty", "html:target/positive/cucumber.html","com.cucumber.listener.ExtentCucumberFormatter"},
+        plugin = {"json:target/positive/cucumber.json", 
+        		  "pretty", "html:target/positive/cucumber.html","com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},
         features = "src/test/resources/features",
         glue = {"steps"}
-        ,tags = {"@Login, @Search-Cars, @Used-Search-Cars"}       
+        ,tags = {"@Login"}       
         )
 public class RunCuke extends AbstractTestNGCucumberTests {
 	
+	String screenshotdir = System.getProperty("user.dir") + "\\test-output\\Screenshots\\";
+	
 	@BeforeClass
-    public static void setup() {
-        // Initiates the extent report and generates the output in the output/Run_<unique timestamp>/report.html file by default.
-		SimpleDateFormat sdf = new SimpleDateFormat("ddMMyyyy_hhmmss");
-		Date curDate = new Date();
-		String strDate = sdf.format(curDate);
-		String fileName = System.getProperty("user.dir")+"\\target\\Extent_Reports\\" + strDate+".html";
-		File newFile = new File(fileName);
-		ExtentCucumberFormatter.initiateExtentCucumberFormatter(newFile,false);
-		//static report name
-		//ExtentCucumberFormatter.initiateExtentCucumberFormatter(new File("F:\\cucumber-testing-master\\ExtenReports\\extentreports.html"),false);
-        // Loads the extent config xml to customize on the report.
-        ExtentCucumberFormatter.loadConfig(new File("src/test/resources/extent-config.xml"));
+    public void setup() throws IOException {
+        
+		if((new File(screenshotdir)).exists()) {
+			FileUtils.cleanDirectory(new File(screenshotdir));
+		}
 
-        // User can add the system information as follows
-        ExtentCucumberFormatter.addSystemInfo("Browser Name", "Chrome");
-        ExtentCucumberFormatter.addSystemInfo("Browser version", "v31.0");
-        ExtentCucumberFormatter.addSystemInfo("Selenium version", "v3.8.1");
 
-        // Also you can add system information using a hash map
-        Map systemInfo = new HashMap();
-        systemInfo.put("Cucumber version", "v1.2.3");
-        systemInfo.put("Extent Cucumber Reporter version", "v1.1.0");
-        ExtentCucumberFormatter.addSystemInfo(systemInfo);
     }
 	
 	
 	@AfterClass
-	public static void teardown() {
+	public void teardown() {
 		
 		
 	}
--- CucumberBasics/src/test/java/steps/AfterActions.java
+++ CucumberBasics/src/test/java/steps/AfterActions.java
@@ -9,11 +9,14 @@ import utils.SeleniumDriver;
 
 public class AfterActions {
 	
+	String screenshotdir = System.getProperty("user.dir") + "/test-output/Screenshots/";
+	
+	
 	@After
 	public static void tearDown(Scenario scenario) {
 		
 		WebDriver driver=SeleniumDriver.getDriver();
-    	System.out.println(scenario.isFailed());
+    	
     	 if (scenario.isFailed()) {
              byte[] screenshotBytes = ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
              scenario.embed(screenshotBytes, "image/png");
